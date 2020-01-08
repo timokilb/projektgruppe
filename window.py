5af15ae43699ee00ce045b05d45ed5a78d07c33a
nagod <deniz-dogan@t-online.de>
2020-01-08 19:18:07 +0100

    first stable responsive design

--- window.py
+++ window.py
@@ -199,13 +199,13 @@ def clear_command():
     graph_list_index = 0
 
     if algorithm.get() == "Treap":
-        treap_graph_list = []
+        treap_graph_list.clear()
         treap = tr.Treap()
         treap_graph = TreapGraph(treap)
         treap_graph_list.append(treap_graph.create_graph())
         update_canvas(treap_graph_list[graph_list_index])
     elif algorithm.get() == "Skip List":
-        skip_list_graph_list = []
+        skip_list_graph_list.clear()
         skip_list = sl.SkipList()
         skip_list_graph = SkipListGraph(skip_list)
         skip_list_graph_list.append(skip_list_graph.create_graph(skip_list))
@@ -302,6 +302,7 @@ def save_file():
     if filename:
         fig.savefig(filename)
 
+
 def callor(event):
     insert_command()
 
@@ -309,26 +310,27 @@ def callor(event):
 def placeholder(event):
     value_entry.delete(0, tk.END)
 
-# TODO Mit getter und setter arbeiten!
 
+button_styles = {
+    "animation_button": {
+        "fg": "#1a1a1a",
+        "bg": "#8a8a8a",
+        "font": "Helvetica, 12",
+        "padx": "10",
+        "pady": "0"
+    },
+    "data_structure_button": {
+        "fg": "#1a1a1a",
+        "bg": "#8a8a8a",
+        "font": "Helvetica, 12",
+        "padx": "1",
+        "pady": "0"
+    },
+    "log_text": {
+        "font": "ariel,30"
+    }
+}
 if __name__ == "__main__":
-    # StyleSheet
-
-    background_color = "#3a3a3a"
-    button_color = "#8a8a8a"
-    label_color = "#a2a2a2"
-    button_label_color = "#1a1a1a"
-
-    # Main window
-    root = tk.Tk()
-    root.title("Getting laid Vol .4 mit Kohout von der Salbe 4")
-    root.config(background=background_color)
-    # TODO : Responsive via coord.
-    # Figure and plot
-    fig = Figure(figsize=(10, 5), facecolor="grey",
-                 dpi=100)
-    plot = fig.add_subplot(111)  # 1 by 1 grid subplot No. 1
-
     # Init data structures and graphs
     graph_list_index = 0
     skip_list_graph_list = []
@@ -339,38 +341,58 @@ if __name__ == "__main__":
     treap = tr.Treap()
     treap_graph = TreapGraph(treap)
 
-    # Frame for displaying operators
-    container_interface = tk.Frame(root, bg="green")
-    container_interface.pack(side="top", fill="x")
+    # Array for all numbers from Input txt
+    data = []
+    #Background Color
+    background_color = "#3a3a3a"
+    # Figure and plot
 
-    log_frame = tk.LabelFrame(container_interface, text="fickerjackson", font="helvetica")
-    canvas_frame = tk.Frame(root, bg=background_color, padx="10")
-    fetten_container = tk.Frame(container_interface, bg="red")
+    fig = Figure(figsize=(10, 5), facecolor="grey", dpi=100)
+    plot = fig.add_subplot(111)  # 1 by 1 grid subplot No. 1
 
-    animation_frame = tk.Frame(fetten_container, bg=background_color, relief="ridge", bd="3")
 
-    data_structure_frame = tk.Frame(root, bg=background_color)
-    key_structure_frame = tk.Frame(data_structure_frame, bg=background_color)
-    operator_frame = tk.Frame(data_structure_frame, bg=background_color)
+    # Making the window elements
+    # Main window
+    root = tk.Tk()
+    root.title("Getting laid Vol .4 mit Kohout von der Salbe 4")
+    root.config(background=background_color)
 
-    graph_structure_frame = tk.Frame(root, bg=background_color)
-    switch_algorithm_frame = tk.Frame(graph_structure_frame, bg=background_color)
-    graph_operation_frame = tk.Frame(graph_structure_frame, bg=background_color)
-
-    # Canvas for drawing the list/ treap
+    # Canvas frame
+    canvas_frame = tk.Frame(master=root, bg=background_color)
     canvas = FigureCanvasTkAgg(fig, master=canvas_frame)
-    canvas._tkcanvas.pack()
-    # canvas._tkcanvas.grid(row=0, column=0, columnspan=4)
-    skip_list_graph.draw(skip_list, plot, canvas)
 
-    # Canvas for displaying the Pseudocode
-    pseudo_canvas = tk.Canvas(root, width=400, height=350, background="red")
-    # pseudo_canvas.grid(row=0, column=4, columnspan=2, padx=0, pady=0)  # Use sticky for sticking it to the top
+    # Bottom half sub frames
+    interface_frame = tk.Frame(master=root, bg="green")
+
+    # The log
+    log_frame = tk.Frame(master=interface_frame) # text="fickerjackson", font="helvetica")
+
+    # Frame that contains every button
+    button_frame = tk.Frame(master=interface_frame, bg="red")
+    animation_frame = tk.Frame(master=button_frame, bg=background_color, relief="ridge", bd="3")
+
+    data_structure_frame = tk.Frame(master=button_frame, bg=background_color)
+    key_structure_frame = tk.Frame(master=data_structure_frame, bg=background_color)
+    operator_frame = tk.Frame(master=data_structure_frame, bg=background_color)
+
+    graph_structure_frame = tk.Frame(master=button_frame, bg=background_color)
+    switch_algorithm_frame = tk.Frame(master=graph_structure_frame, bg=background_color)
+    graph_operation_frame = tk.Frame(master=graph_structure_frame, bg=background_color)
+
+
+
+    # Pseudocode canvas
+    pseudo_canvas = tk.Canvas(master=interface_frame, width=400, height=350, background="red")
     img = ImageTk.PhotoImage(Image.open("./res/testpesudocode.jpeg"))
     pseudo_canvas.create_image(203, 253, image=img)
 
-    # Array for all numbers from Input txt
-    data = []
+
+
+
+    interface_frame.pack(side="top", fill="x")
+
+
+    skip_list_graph.draw(skip_list, plot, canvas)
 
     # Dropdown menu for choosing the algorithm
     algorithms = ["Skip List", "Treap"]
@@ -380,112 +402,92 @@ if __name__ == "__main__":
 
     # Buttons
 
-    button_styles = {
-        "animation_button": {
-            "fg": "#1a1a1a",
-            "bg": "#8a8a8a",
-            "font": "Helvetica, 12",
-            "padx": "10",
-            "pady": "0"
-        },
-        " data_structure_button": {
-            "fg": "#1a1a1a",
-            "bg": "#8a8a8a",
-            "font": "Helvetica, 12",
-            "padx": "1",
-            "pady": "0"
-        },
-        "log_text": {
-            "font": "ariel,30"
-        }
-    }
-
-    play_pause_button = tk.Button(animation_frame, text="Play", command=play_command,
+    play_pause_button = tk.Button(master=animation_frame, text="Play", command=play_command,
                                   fg=button_styles["animation_button"]["fg"],
                                   bg=button_styles["animation_button"]["bg"],
                                   font=button_styles["animation_button"]["font"])
 
-    previous_button = tk.Button(animation_frame, text="Previous",
+    previous_button = tk.Button(master=animation_frame, text="Previous",
                                 command=previous_command,
                                 fg=button_styles["animation_button"]["fg"],
                                 bg=button_styles["animation_button"]["bg"],
                                 font=button_styles["animation_button"]["font"])
 
-    next_button = tk.Button(animation_frame, text="Next", command=next_command,
+    next_button = tk.Button(master=animation_frame, text="Next", command=next_command,
                             fg=button_styles["animation_button"]["fg"],
                             bg=button_styles["animation_button"]["bg"],
                             font=button_styles["animation_button"]["font"])
 
-    stop_button = tk.Button(animation_frame, text="Stop", command=stop_command,
+    stop_button = tk.Button(master=animation_frame, text="Stop", command=stop_command,
                             fg=button_styles["animation_button"]["fg"],
                             bg=button_styles["animation_button"]["bg"],
                             font=button_styles["animation_button"]["font"])
 
-    value_label = tk.Label(key_structure_frame, text="Key", fg=label_color, bg=background_color, font=("Helvetica", 22))
-    value_entry = tk.Entry(key_structure_frame, width=50)
+    value_label = tk.Label(master=key_structure_frame, text="Key", fg="#1a1a1a", bg=background_color, font=("Helvetica", 12))
+    value_entry = tk.Entry(master=key_structure_frame, width=50)
     value_entry.insert(0, "\tEnter a KEY to perform an operation")
 
-    search_button = tk.Button(operator_frame, text="Search", command=search_command)
-    insert_button = tk.Button(operator_frame, text="Insert", fg=button_label_color, command=insert_command)
-    delete_button = tk.Button(operator_frame, text="Delete", fg=button_label_color, command=delete_command)
+    search_button = tk.Button(master=operator_frame, text="Search", command=search_command)
+    insert_button = tk.Button(master=operator_frame, text="Insert", fg=button_styles["data_structure_button"]["fg"], command=insert_command)
+    delete_button = tk.Button(master=operator_frame, text="Delete", fg=button_styles["data_structure_button"]["fg"], command=delete_command)
 
-    open_button = tk.Button(graph_operation_frame, text="Open File", fg=button_label_color, bg=button_color,
+    open_button = tk.Button(master=graph_operation_frame, text="Open File", fg=button_styles["data_structure_button"]["fg"], bg=button_styles["data_structure_button"]["bg"],
                             command=open_file)
-    clear_button = tk.Button(graph_operation_frame, text="Clear Graph", fg=button_label_color, bg=button_color,
+    clear_button = tk.Button(master=graph_operation_frame, text="Clear Graph", fg=button_styles["data_structure_button"]["fg"], bg=button_styles["data_structure_button"]["bg"],
                              command=clear_command)
-    save_button = tk.Button(graph_operation_frame, text="Save Graph", fg=button_label_color, bg=button_color,
+    save_button = tk.Button(master=graph_operation_frame, text="Save Graph", fg=button_styles["data_structure_button"]["fg"], bg=button_styles["data_structure_button"]["bg"],
                             command=save_file)
-    filename_label = tk.Label(graph_operation_frame, text="FILENAME", width=20, relief="sunken")
+    filename_label = tk.Label(master=graph_operation_frame, text="FILENAME", width=20, relief="sunken")
 
     info_button = tk.Button(root, text="?", fg="red", bg="green", command=info_command, relief="raised", bitmap="info")
     # B3 = Button(top, text="info", relief=RAISED,\ bitmap = "info")
     # Testing log output
     log_widget = lw.LogWidget()
-    log_message = tk.Message(log_frame, text=log_widget.update(), width=500, font=button_styles["log_text"]["font"])
+    log_message = tk.Message(master=log_frame, text=log_widget.update(), width=500, font=button_styles["log_text"]["font"])
 
-    # Grid frame layout
-    # canvas_frame.grid(row=0, column=0, columnspan=4)
-    canvas_frame.pack(side="top")
-    container_interface = tk.Frame(root, bg="green")
-    container_interface.pack(side="bottom")
+    # Packing everything
+    canvas_frame.pack(side="top", fill="both", expand=100, anchor="e", padx=10, pady=10)
+    canvas._tkcanvas.pack(fill="both", expand=100, anchor="e")
+
+    interface_frame.pack(side="bottom")
+
     log_frame.pack(side="left")
-    fetten_container.pack(side="left")
+    log_message.pack(side="left")
+
+    button_frame.pack(side="left")
+    animation_frame.pack(side="top")
+    data_structure_frame.pack(side="top")
+    key_structure_frame.pack(side="top")
+    operator_frame.pack(side="top")
+
+    graph_structure_frame.pack(side="top")
+    switch_algorithm_frame.pack(side="top")
+    graph_operation_frame.pack(side="bottom")
+
     pseudo_canvas.pack(side="left")
-    log_message.pack()
-    info_button.pack()
+#    info_button.pack()
 
-    # log_frame.grid(row=1, column=4, rowspan=6, columnspan=2, sticky="NW")
-    """animation_frame.grid(row=1, column=0, columnspan=9, sticky="NW")
-    data_structure_frame.grid(row=2, column=3)
-    key_structure_frame.grid(row=0, column=0, columnspan=2)
-    operator_frame.grid(row=1, column=0, columnspan=2)
-    graph_structure_frame.grid(row=2, column=0, columnspan=2)
-    switch_algorithm_frame.grid(row=0, column=0, columnspan=2)
-    graph_operation_frame.grid(row=1, column=0, columnspan=2)
+    # Packing all buttons
+    stop_button.pack(side="left")
+    previous_button.pack(side="left")
+    play_pause_button.pack(side="left")
+    next_button.pack(side="left")
 
-    # Layout
-    play_pause_button.grid(row=0, column=2, padx=button_styles["animation_button"]["padx"])
-    previous_button.grid(row=0, column=1, padx=button_styles["animation_button"]["padx"])
-    next_button.grid(row=0, column=3, padx=button_styles["animation_button"]["padx"])
-    stop_button.grid(row=0, column=0, padx=button_styles["animation_button"]["padx"])
+    value_label.pack(side="left")
+    value_entry.pack(side="left")
 
-    value_label.grid(row=0, column=0, sticky="E")
-    value_entry.grid(row=0, column=1)
-    search_button.grid(row=2, column=0, columnspan=2)
-    insert_button.grid(row=0, column=0, columnspan=2)
-    delete_button.grid(row=1, column=0, columnspan=2)
+    insert_button.pack(side="top")
+    delete_button.pack(side="top")
+    search_button.pack(side="top")
 
-    algo_dropdown.grid(row=0, column=0, columnspan=2)
-    open_button.grid(row=0, column=0)
-    filename_label.grid(row=0, column=1)
-    clear_button.grid(row=1, column=0)
-    save_button.grid(row=1, column=1)
+    algo_dropdown.pack(side="left")
+    open_button.pack(side="top")
+    filename_label.pack(side="top")
+    clear_button.pack(side="top")
+    save_button.pack(side="top")
 
-    log_message.grid(row=0, column=0)
-    info_button.grid(row=2, column=5, sticky="SE")
-    """
+
     value_entry.bind("<Button>", placeholder)
-    # TODO : auskommi weil sonst nur ein entry möglich , try it !
     # value_entry.bind("<Key>", placeholder)
     root.bind('<Return>', callor)
 
